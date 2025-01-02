af0e7a3bc6f4b561f3aff098b5fdb65ed084a94a
Evelina <110685233+EvelinaAnil@users.noreply.github.com>
2025-01-02 18:08:35 +0200

    Delete Another java directory

--- Another java/_HW_SpringBoot_Android_/_HW_SpringBoot_Android_/MyWork/backend-android/src/main/java/edu/itstep/backendandroid/respository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.itstep.backendandroid.respository;
-
-import edu.itstep.backendandroid.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findUserByLogin(String login);
-
-}
